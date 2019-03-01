34983eea9c3afbefde76610b4a239182c5e88973
Torsten Bögershausen <torsten.bogershausen@esss.se>
2019-03-01 15:30:43 +0100

    indexer: info type 0 working (?)

--- test/simulator/EtherCAT/indexer.c
+++ test/simulator/EtherCAT/indexer.c
@@ -1,3 +1,7 @@
+/* Implementation of the indexer
+  https://forge.frm2.tum.de/public/doc/plc/master/singlehtml/index.html#devices
+ */
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -7,13 +11,28 @@
 #include "indexer.h"
 
 
-#define INDEXEROFFSET    62
+/* In the memory bytes, the indexer starts at 64 */
+#define INDEXEROFFSET    64
+/*  Device 0 is at here, leaving some space */
 #define DEVICE0_OFFSET  100
+
+/* Info types of the indexer */
+typedef struct {
+    uint16_t   typeCode;
+    uint16_t   size;
+    uint16_t   offset;
+    uint16_t   unit;
+    uint16_t   flagsLow;
+    uint16_t   flagsHigh;
+    float      absMin;
+    float      absMax;
+} indexerInfoType0_type;
 
 typedef struct {
   uint16_t  typeCode;
   uint16_t  size;
   uint16_t  offset;
+  uint16_t  unit;
   uint8_t   flags;
   uint32_t  allFlags;
   uint16_t  parameters[16]; /* counting 0..15 */
@@ -24,24 +43,31 @@ typedef struct {
 } indexerDeviceAbsStraction_type;
 
 indexerDeviceAbsStraction_type indexerDeviceAbsStraction[1] =
-  {
-    { 0x5008, 0x08,  DEVICE0_OFFSET, 0,
-      0,
-      {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-      "motor1",
-      { "", "", "", "", "", "", "", "" },
-      5.0, 175.0 }
-  };
+{
+  { 0x5008, 0x08,  DEVICE0_OFFSET, 0xc04, 0,
+    0,
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    "motor1",
+    { "", "", "", "", "", "", "", "" },
+    5.0, 175.0
+  }
+};
 
 
 
 
 static union {
-  uint8_t memoryBytes[1024];
+  uint8_t  memoryBytes[1024];
+  uint16_t memoryWords[512];
   struct {
     float    magic;
     uint16_t offset;
-  } memoryStruct;
+    uint16_t persistant_unused[58]; /* must match INDEXEROFFSET */
+    /* Area for the indexer. union of the different info types */
+    union {
+      indexerInfoType0_type infoType0;
+      } indexer;
+    } memoryStruct;
 } idxData;
 
 static int initDone = 0;
@@ -61,11 +87,40 @@ static int indexerHandleIndexerCmd(unsigned indexOffset,
                                    unsigned uValue)
 {
   unsigned devNum = uValue & 0xFF;
-  unsigned infoType = (uValue >> 8) & 0xFF;
+  unsigned infoType = (uValue >> 8) & 0x7F;
   LOGINFO3("%s/%s:%d indexOffset=%u len_in_PLC=%u uValue=%u devNum=%u infoType=%u\n",
            __FILE__, __FUNCTION__, __LINE__,
            indexOffset, len_in_PLC,
            uValue, devNum, infoType);
+  if (devNum > (sizeof(indexerDeviceAbsStraction)/
+                sizeof(indexerDeviceAbsStraction[0]))) {
+    return __LINE__;
+  }
+  memset(&idxData.memoryStruct.indexer, 0, sizeof(idxData.memoryStruct.indexer));
+  switch (infoType) {
+    case 0:
+      if (!devNum) {
+        /* The indexer himself. */
+        idxData.memoryStruct.indexer.infoType0.offset = INDEXEROFFSET;
+        idxData.memoryStruct.indexer.infoType0.flagsHigh = 0x8000; /* extended indexer */
+      } else {
+        /* get values from device table */
+        unsigned devNo = devNum -1;
+        idxData.memoryStruct.indexer.infoType0.typeCode = indexerDeviceAbsStraction[devNo].typeCode;
+        idxData.memoryStruct.indexer.infoType0.size = indexerDeviceAbsStraction[devNo].size;
+        idxData.memoryStruct.indexer.infoType0.offset = 100 + 16 * devNo; /* TODO */
+        idxData.memoryStruct.indexer.infoType0.unit = indexerDeviceAbsStraction[devNo].unit;
+        /* TODO: calc the flags from lenght of AUX strings */
+        //idxData.memoryStruct.indexer.infoType0.flagsLow = indexerDeviceAbsStraction[devNo].flagsLow;
+        //idxData.memoryStruct.indexer.infoType0.flagsHigh = indexerDeviceAbsStraction[devNo].flagsHigh;
+        idxData.memoryStruct.indexer.infoType0.absMin = indexerDeviceAbsStraction[devNo].absMin;
+        idxData.memoryStruct.indexer.infoType0.absMax = indexerDeviceAbsStraction[devNo].absMax;
+      }
+      idxData.memoryWords[INDEXEROFFSET / 2] |= 0x8000; /* ACK */
+      return 0;
+    default:
+      return __LINE__;
+    }
   return __LINE__;
 }
 
