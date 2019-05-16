3c485503f5797dc2dabb8450f2bf1383fd76a5fe
Torsten Bögershausen <torsten.bogershausen@esss.se>
2019-05-16 09:05:41 +0200

    TC 200: fix timing problems
    The test in 200_Ethercat-powerOff-JOG-CNEN-Error.py was often leaving
    the real hardware in a non-usable state.
    Power was off and the error was still there.
    
    Add polling loops when the power is switched on or off
    and add a loop when the -ErrRst bit is set.
    
    While there, rename the test cases into 200x for consistancy.

--- test/nose_tests/200_Ethercat-powerOff-JOG-CNEN-Error.py
+++ test/nose_tests/200_Ethercat-powerOff-JOG-CNEN-Error.py
@@ -37,50 +37,48 @@ class Test(unittest.TestCase):
     saved_PwrAuto = epics.caget(motor + '-PwrAuto')
 
     # 10% UserPosition
-    def test_TC_201(self):
+    def test_TC_2001(self):
         motor = self.motor
-        tc_no = "TC-201-10-percent-dialPosition"
+        tc_no = "TC-2001-10-percent-dialPosition"
         print '%s' % tc_no
         epics.caput(motor + '.CNEN', 1)
+        self.lib.waitForPowerOn(motor, tc_no, 8.0)
         destination =  (1 * self.saved_HLM + 9 * self.saved_LLM) / 10
         epics.caput(motor + '.VAL', destination, wait=True)
 
 
     # Jog, wait for start, power off, check error, reset error
-    def test_TC_202(self):
+    def test_TC_2002(self):
         motor = self.motor
-        tc_no = "TC-202-JOG-_Enable"
+        tc_no = "TC-2002-JOG-_Enable"
         epics.caput(motor + '-PwrAuto', 0)
         epics.caput(motor + '.CNEN', 0)
+        self.lib.waitForPowerOff(motor, tc_no, 8.0)
         epics.caput(motor + '.JOGF', 1)
-        ret = self.lib.waitForStart(motor, tc_no, 2.0)
         # dummy wait
+        ret_0 = self.lib.waitForStart(motor, tc_no, 2.0)
 
-        ret = self.lib.waitForStop(motor, tc_no, 4.0)
-        self.assertEqual(True, ret, 'waitForStop return True')
+        ret_1 = self.lib.waitForStop(motor, tc_no, 10.0)
 
-        msta = int(epics.caget(motor + '.MSTA', use_monitor=False))
-        mstaErr = int(epics.caget(motor + '.MSTA', use_monitor=False))
-        print '%s Error mstaErr=%s' % (tc_no, self.lib.getMSTAtext(mstaErr))
-        self.assertNotEqual(0, msta & self.lib.MSTA_BIT_PROBLEM, 'Error MSTA.Problem should be set)')
-        self.assertEqual(0, msta & self.lib.MSTA_BIT_SLIP_STALL, 'Error MSTA.Slip stall Error should not be set)')
-        self.assertEqual(0, msta & self.lib.MSTA_BIT_MOVING,     'Error MSTA.Moving)')
+        msta_1 = int(epics.caget(motor + '.MSTA', use_monitor=False))
+        mstaErr_1 = int(epics.caget(motor + '.MSTA', use_monitor=False))
 
-        bError   = self.pv_Err.get(use_monitor=False)
-        nErrorId = self.pv_nErrorId.get(use_monitor=False)
-        print '%s Error bError=%d nErrorId=%d' % (tc_no, bError, nErrorId)
-
-        self.assertNotEqual(0, bError,   'bError')
-        self.assertNotEqual(0, nErrorId, 'nErrorId')
+        bError_2   = self.pv_Err.get(use_monitor=False)
+        nErrorId_2 = self.pv_nErrorId.get(use_monitor=False)
+        print '%s Error bError_2=%d nErrorId_2=%d' % (tc_no, bError_2, nErrorId_2)
 
         self.pv_nErrRst.put(1)
 
-        msta = int(self.pv_MSTA.get(use_monitor=False))
-        bError   = self.pv_Err.get(use_monitor=False)
-        nErrorId = self.pv_nErrorId.get(use_monitor=False)
-        print '%s Clean self.lib.MSTA_BIT_PROBLEM=%x msta=%x bError=%d nErrorId=%d' % (tc_no, self.lib.MSTA_BIT_PROBLEM, msta, bError, nErrorId)
+        msta_3 = int(self.pv_MSTA.get(use_monitor=False))
+        bError_3   = self.pv_Err.get(use_monitor=False)
+        nErrorId_3 = self.pv_nErrorId.get(use_monitor=False)
+        print '%s Clean self.lib.MSTA_BIT_PROBLEM=%x msta_3=%x bError_3=%d nErrorId=%d' % (tc_no, self.lib.MSTA_BIT_PROBLEM, msta_3, bError_3, nErrorId_3)
 
+        # Loop until moving has stopped and error has been reseted
         counter = 7
+        msta = msta_3
+        nErrorId = nErrorId_3
+        bError = bError_3
         while (msta & self.lib.MSTA_BIT_MOVING or bError != 0 or nErrorId != 0):
             time.sleep(polltime)
             print '%s sleep counter = %d' % (tc_no, counter)
@@ -90,9 +88,26 @@ class Test(unittest.TestCase):
             counter = counter - 1
             if counter == 0:
                 break
-
+        # Restore the values we had before this test started
         epics.caput(motor + '.CNEN', self.saved_CNEN)
+        if self.saved_CNEN:
+            self.lib.waitForPowerOn(motor, tc_no + "restore", 8.0)
+        else:
+            self.lib.waitForPowerOff(motor, tc_no+ "restore", 3.0)
+
         epics.caput(motor + '-PwrAuto', self.saved_PwrAuto)
+
+        # Run all the asserts after we have restored the original state
+        self.assertEqual(True, ret_1, 'waitForStop return True')
+
+        print '%s Error mstaErr_1=%s' % (tc_no, self.lib.getMSTAtext(mstaErr_1))
+        self.assertNotEqual(0, msta_1 & self.lib.MSTA_BIT_PROBLEM, 'Error MSTA.Problem should be set)')
+        self.assertEqual(0, msta_1 & self.lib.MSTA_BIT_SLIP_STALL, 'Error MSTA.Slip stall Error should not be set)')
+        self.assertEqual(0, msta_1 & self.lib.MSTA_BIT_MOVING,     'Error MSTA.Moving)')
+
+        self.assertNotEqual(0, bError_2,   'bError')
+        self.assertNotEqual(0, nErrorId_2, 'nErrorId')
+
         self.assertEqual(0, msta & self.lib.MSTA_BIT_MOVING,  'Clean MSTA.Moving)')
         self.assertEqual(0, bError,   'bError')
         self.assertEqual(0, nErrorId, 'nErrorId')
